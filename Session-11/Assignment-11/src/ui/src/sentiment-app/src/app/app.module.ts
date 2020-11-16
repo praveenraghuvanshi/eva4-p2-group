922969a9c5337d7ff58075abaf7135f1901d4b18
Praveen Raghuvanshi <praveenraghuvanshi@gmail.com>
2020-11-16 10:14:54 +0530

    Removed app Menu

--- Session-11/Assignment-11/src/ui/src/sentiment-app/src/app/app.module.ts
+++ Session-11/Assignment-11/src/ui/src/sentiment-app/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { TranslationComponent } from './translation/translation.component';
-import { MenuComponent } from './menu/menu.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
@@ -12,7 +11,6 @@ import { NgtUniversalModule } from '@ng-toolkit/universal';
 @NgModule({
    declarations: [
       AppComponent,
-      MenuComponent,
       TranslationComponent
    ],
    imports: [
