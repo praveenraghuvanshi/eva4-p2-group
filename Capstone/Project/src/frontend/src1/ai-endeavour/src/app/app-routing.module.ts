1a8b13c70b67d5e06d63d5876bc9c89c325a959e
Praveen Raghuvanshi <praveenraghuvanshi@gmail.com>
2020-12-25 04:46:20 +0530

    Added Menu and SentimentAnalysis module

---
+++ Capstone/Project/src/frontend/src1/ai-endeavour/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SentimentAnalysisComponent } from './sentiment-analysis/sentiment-analysis.component';
+
+@NgModule({
+    imports: [
+      RouterModule.forRoot([
+        { path : '', redirectTo : 'sentimentAnalysisComponent', pathMatch: 'full' },
+        { path : 'sentimentAnalysisComponent', component : SentimentAnalysisComponent}
+      ])
+    ],
+    exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
--- Capstone/Project/src/frontend/src1/ai-endeavour/src/app/app.module.ts
+++ Capstone/Project/src/frontend/src1/ai-endeavour/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SentimentAnalysisComponent } from './sentiment-analysis/sentiment-analysis.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SentimentAnalysisComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
