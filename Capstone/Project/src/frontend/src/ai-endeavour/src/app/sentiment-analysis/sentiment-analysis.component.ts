00599613884c9bd2999d5a7c4744ef1bc0dd542c
Praveen Raghuvanshi <praveenraghuvanshi@gmail.com>
2020-12-25 02:23:02 +0530

    Adde csv upload

--- Capstone/Project/src/frontend/src/ai-endeavour/src/app/sentiment-analysis/sentiment-analysis.component.ts
+++ Capstone/Project/src/frontend/src/ai-endeavour/src/app/sentiment-analysis/sentiment-analysis.component.ts
@@ -7,7 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SentimentAnalysisComponent {
 
-  constructor() {
-    console.log("Inside SentimentAnalysis ctor()");
+  file:File = null!;
+  isProcessing = false;
+  isTrained = false;
+  filePreview:string = '';
+  sentimentSentence = '';
+
+  constructor() {  }
+
+  onSelectFile(event: Event) { // called each time file input changes
+    const input = event.target as HTMLInputElement;
+    if (!input.files?.length) {
+      return;
+    }
+
+    this.file = input.files[0];
+    console.log(this.file);
+    var reader = new FileReader();
+    // reader.readAsDataURL(this.file); // read file as data url
+    reader.readAsText(this.file);
+    reader.onload = (event: Event) => { // called once readAsDataURL is completed
+        this.filePreview = reader.result as string;;
+    }
+  }
+
+  trainModel(){
+    console.log("Training started...");
+    this.isTrained = false;
+    this.isProcessing = true;
+    setTimeout(() => {
+      console.log('hello');
+      console.log("Training Completed...");
+      this.isTrained = true;
+      this.isProcessing = false;
+    }, 5000);
+  }
+
+  predictSentiment(value: string): void {
+    this.sentimentSentence = `${value}`;
+    console.log('Sentiment Sentence: ' + this.sentimentSentence);
+    this.isProcessing = true;
   }
 }
