542c40875d2ed432270c8ba53ee4d879f00deea3
Praveen Raghuvanshi <praveenraghuvanshi@gmail.com>
2020-12-25 05:52:23 +0530

    deployment code added

--- Capstone/Project/src/frontend/src1/ai-endeavour/src/app/app.component.ts
+++ Capstone/Project/src/frontend/src1/ai-endeavour/src/app/app.component.ts
@@ -1,3 +1,10 @@
+;
+import { Inject } from '@angular/core';
+import { PLATFORM_ID } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { isPlatformBrowser } from '@angular/common';
+import { OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +12,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ai-endeavour';
+public ngOnInit(): void {
+    if (!isPlatformBrowser(this.platformId)) {
+        const bases = this.document.getElementsByTagName('base');
+
+        if (bases.length > 0) {
+            bases[0].setAttribute('href', environment.baseHref);
+        }
+    }
+}
+
+
+ constructor(@Inject(PLATFORM_ID) private platformId: any, @Inject(DOCUMENT) private document: any) {}
 }
