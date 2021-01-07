c4c5b50ac9c9bf3b247a7c05124aa63186adc5ae
Praveen Raghuvanshi <praveenraghuvanshi@gmail.com>
2021-01-08 02:19:13 +0530

    Image upload

--- Capstone/Project/src/frontend/src/ai-endeavour/src/app/image-classification/image-classification.component.ts
+++ Capstone/Project/src/frontend/src/ai-endeavour/src/app/image-classification/image-classification.component.ts
@@ -6,6 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./image-classification.component.css']
 })
 export class ImageClassificationComponent {
+  projectname = ''
+  images = []
 
   constructor() { }
+
+  OnSelectFolder(event){
+    if (event.target.files && event.target.files[0]){
+      var files = event.target.files;
+      var path = files[0].webkitRelativePath;
+      var Folder = path.split("/");
+      console.log('Selected Folder: ' + Folder[0]);
+      var filesCount = files.length;
+      var previewCount = filesCount <= 10  ? filesCount : 10;
+      for (let i = 0; i < previewCount; i++) {
+              var reader = new FileReader();
+              reader.onload = (event:any) => {
+                this.images.push(event.target.result);
+              }
+              reader.readAsDataURL(event.target.files[i]);
+      }
+    }
+  }
 }
