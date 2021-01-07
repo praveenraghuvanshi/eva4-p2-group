57acf39374be02daf062a55e9a69414ee2a350df
Praveen Raghuvanshi <praveenraghuvanshi@gmail.com>
2021-01-07 19:38:07 +0530

    FlaskApp - Added CORS handling for POST request

--- Capstone/Project/src/backend/flaskpp/src/app.py
+++ Capstone/Project/src/backend/flaskpp/src/app.py
@@ -3,7 +3,7 @@ import io
 import json
 import re
 from flask import Flask
-from flask import request
+from flask import request, jsonify, make_response
 from train import train_model, predict_sentiment, upload_data
 from werkzeug.utils import secure_filename
 
@@ -37,15 +37,32 @@ def train():
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    sentence  = request.json['inputtext']
-    modelName = request.json['model']
-    textFields = request.json["textfields"]
-    
-    predValue = predict_sentiment(sentence, modelName, textFields)
-    print('Predicted value',predValue)
-    review = "Positive" if predValue >= 0.5 else "Negative"
-    print(review)
-    return review
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_prelight_response()
+    elif request.method == "POST": # The actual request following the preflight
+        sentence  = request.json['inputtext']
+        modelName = request.json['model']
+        textFields = request.json["textfields"]
+        
+        predValue = predict_sentiment(sentence, modelName, textFields)
+        print('Predicted value',predValue)
+        review = "Positive" if predValue >= 0.5 else "Negative"
+        print(review)
+        response = make_response()
+        return _corsify_actual_response(jsonify({'prediction': review}))
+    else:
+        raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
+
+def _build_cors_prelight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
